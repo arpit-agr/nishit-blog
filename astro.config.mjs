8ce3000c8e5a3d79fdce0539f46e4676ab113f75
arpit-agr <arpit.agrawal2502@gmail.com>
2025-01-22 14:27:45 +0530

    add site info to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	//TODO: update the final, deployed URL
+	site: "https://nishitjalan.netlify.app",
+});
