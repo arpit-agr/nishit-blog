9a9e4e5d58e0d14d9ac8e3f51135ea1a7805ede1
arpit-agr <arpit.agrawal2502@gmail.com>
2025-02-01 14:43:30 +0530

    add blog post image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
 	//TODO: update the final, deployed URL
 	site: "https://nishitjalan.netlify.app",
-
 	integrations: [mdx()],
+	markdown: {
+		rehypePlugins: [rehypeUnwrapImages],
+	},
 });
