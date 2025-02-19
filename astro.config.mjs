340758a96eeb115c4b71abf9ebf001ea87933bc8
arpit-agr <arpit.agrawal2502@gmail.com>
2025-02-19 19:35:10 +0530

    add posts archive page, contact page and update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
@@ -8,7 +9,7 @@ export default defineConfig({
 	//TODO: update the final, deployed URL
 	site: "https://nishitjalan.netlify.app",
 	base: "/",
-	integrations: [mdx()],
+	integrations: [mdx(), icon()],
 	markdown: {
 		rehypePlugins: [rehypeUnwrapImages],
 	},
